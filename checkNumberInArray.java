244c0e332cab5c678d664a082ab9fa4c40b5575d
Shivansh Mishra <shivanshmishra@shivanshs-macbook-pro.local>
2018-06-26 18:44:44 +0530

    checkNumberInArray

--- checkNumberInArray.java
+++ checkNumberInArray.java
@@ -1,2 +1,11 @@
 public class checkNumberInArray {
+    public static boolean checkNumber(int input[], int x, int index){
+        if(input[index] == x){
+            return true;
+        }
+        if(index == input.length-1){
+            return false;
+        }
+        return checkNumber(input, x, index+1);
+    }
 }
